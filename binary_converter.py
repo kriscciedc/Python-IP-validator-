f150a788d22b8d7e7d3db0f0db993f1bbab8d14b
Sam Kimber <samkimber@Sams-MacBook-Pro.local>
2016-02-15 07:11:39 -0600

    Code to convert decimal IP address to binary

---
+++ binary_converter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+
+binary_octets = []
+
+if len(sys.argv) != 2:
+	print "Invalid number of arguments"
+else:
+	ip_addr = sys.argv[1]
+	print "The IP address is: %s" % ip_addr
+	octets = ip_addr.split(".")
+	for i in range(len(octets)):
+		current = bin(int(octets[i]))
+		current = current[2:]
+		while len(current) < 8:
+			current = '0' + current
+		binary_octets.append(current)
+binary_addr = ".".join(binary_octets)
+
+print "%-18s %-40s" % ("IP Address","Binary Address")
+print "%-18s %-40s" % (ip_addr, binary_addr)
